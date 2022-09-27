60af36665fdf38158c17cc00cec928d6a8fa7f64
Carina Ogbat <carinawrightogbat@gmail.com>
2022-09-27 16:22:38 -0700

    finished arrays, finished first function

--- arrays.js
+++ arrays.js
@@ -2,15 +2,51 @@
 
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
+  let count = 0
+  for (const item of items) {
+    console.log(`${count} ${item}`);
+    count += 1;
+   
+ 
+  }
 }
 
 // 2. everyOtherItem
+// function everyOtherItem(items) {
+//   const everyOtherList = [];
+//   for (const item in items){
+//     if (item % 2 === 0) {
+//     everyOtherList.push(items[item]);
+//     }
+
+//   console.log(everyOtherList);
+//   }
+// }
+
 function everyOtherItem(items) {
-  // Replace this with your code
+  const resultItems = [];
+
+  for (const i in items) {
+    if (i % 2 === 0) {
+      resultItems.push(items[i]);
+    }
+  }
 }
+
+//   console.log(resultItems);
+// }
+
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+  const sortedItems = items.sort((a, b) => a-b).slice(0, n);
+
+  sortedItems.reverse()
+  console.log(sortedItems);
+
 }
+
+
+const nums = [1, 30, 4, 21, 1000]
+nums.sort((a, b) => a - b);
+console.log(smallestNItems(nums, 3))
--- functions.js
+++ functions.js
@@ -3,6 +3,9 @@
 // 1. isHometown
 
 // Define your function here
+function isHometown(town){
+    return town === "San Francisco"
+}
 
 // 2. getFullName
 
@@ -11,3 +14,8 @@
 // 3. calculateTotal
 
 // Define your function here
+
+const testTown = "Oakland"
+const testTown2 = "San Francisco"
+console.log(isHometown(testTown))
+console.log(isHometown(testTown2))
